1d4e9a9f9ae32943774e9e9fa54cb2f5098c6470
Vitor Paz <kngatuno@gmail.com>
2022-08-26 01:06:28 -0300

    Correção na entrada de dados

--- Acerte o nome.py
+++ Acerte o nome.py
@@ -13,7 +13,7 @@ if resp in 'Ss':
     while i <= 5 and palavra != chave:
         print(f'\033[34mTentativa {i}\033[m')
         palavra = input(
-            f'\033[34mDigite um nome ({len(chave)} letras): \033[m').lower()
+            f'\033[34mDigite um nome ({len(chave)} letras):  \033[m').lower()
         while len(palavra) != len(chave):
             palavra = input(f'\033[34mErro! insira uma palavra com {len(chave)} letras: \033[m').lower()
         for c in range(0, len(chave), 1):
